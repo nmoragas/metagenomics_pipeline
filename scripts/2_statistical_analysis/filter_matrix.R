aad1df21398b084969f02ba11f69af04adda8f6f
nmoragas <n.moragas90@gmail.com>
2025-05-30 10:09:33 +0200

    Update filter_matrix.R

--- scripts/2_statistical_analysis/filter_matrix.R
+++ scripts/2_statistical_analysis/filter_matrix.R
@@ -1,8 +1,1 @@
-# Matrix filter
-filter_matrix = function(count_matrix, relabundance_filter = 0.001, nsamples_filter = 1){
-  # species in columns
-  relabundance_matrix = t(apply(count_matrix, 1, function(x) {return(x/sum(x))}))
-  keep = apply(relabundance_matrix, 2, function(x) {return(length(which(x > relabundance_filter)))})
-  keep = names(keep[keep >= nsamples_filter])
-  return(count_matrix[, keep])
-}
+
